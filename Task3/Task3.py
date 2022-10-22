d83d34ece64aebe99fc51a9ae456b00e0f90cb0d
RogueTMs <roguecyb@gmail.com>
2022-10-23 00:44:32 +0700

    Update Task3.py

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,5 +1,14 @@
 def flatten(arr, depth=-1):
-    pass
+    ans = []
+    for i in arr:
+        if type(i) != list:
+            ans.append(i)
+        else:
+            if depth != 0:
+                ans += flatten(i, depth - 1)
+            else:
+                ans.append(i)
+    return ans
 
 
 print(flatten([1, 2, [4, 5], [6, [7]], 8]))
